1d47c145bac14e86a0ccc90ab804999201174675
Javi Penaloza <veralece@gmail.com>
2024-06-09 14:19:25 -0700

    initial commit of project

---
+++ Source/HombreDeBrincar/HombreDeBrincarGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "HombreDeBrincarGameMode.generated.h"
+
+UCLASS(minimalapi)
+class AHombreDeBrincarGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	AHombreDeBrincarGameMode();
+};
+
+
+
